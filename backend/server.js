4448270fdf996daf330e16aec46b4bcb9630ced4
Lokesh Roopkumar <r.lokesh98@gmail.com>
2021-10-26 23:43:13 -0500

    Adding bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var bcrypt = require('bcryptjs');
 const app = express();
 
 const database = {
@@ -8,7 +9,7 @@ const database = {
             name: 'alan',
             phone: 872184047,
             email: 'alanharper@gmail.com',
-            password: 'stupefy',
+            password: '$2a$08$V9j0xZsn156HE.Qd2DMySuQkkuCJNP1daUX8iM0cXcmwmXp0Mk/k.', //stupefy
             joined: new Date()
         },
         {
@@ -16,7 +17,7 @@ const database = {
             name: 'charlie',
             phone: 872184048,
             email: 'charlieharper@gmail.com',
-            password: 'marco',
+            password: '$2a$08$Wyfha1sSEcz1Uhy2rTL2Y.tsH/Cgxu8s1PdpaD0DkqtZw6LMoLbky',    //marco
             joined: new Date()
         },
         {
@@ -24,7 +25,7 @@ const database = {
             name: 'jake',
             phone: 872184049,
             email: 'jakeharper@gmail.com',
-            password: 'polo',
+            password: '$2a$08$4wFjg6rkLCbpOqUyOczGUu4mDPW7tITgIqrNIpoU9OwRg1y.DdGNm',    //polo
             joined: new Date()
         }
     ]
@@ -33,29 +34,46 @@ const database = {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send(database.users);
+    res.status(200).json(database.users);
 })
 
-app.post('/signin', (req, res) => {
-    const { email, password } = req.body;
-    if (email === database.users[2].email && password === database.users[2].password)
-        res.status(200).json('success');
+app.post('/signin', (request, response) => {
+    const { email, password } = request.body;
+    const emailFound = database.users.some((user, i) => {
+        userFoundAtIndex = i;
+        return user.email === email;
+    })
+    if (emailFound) {
+        bcrypt.compare(password, database.users[userFoundAtIndex].password, (err, res) => {
+            if (res)
+                response.status(200).json(database.users[userFoundAtIndex]);
+            else
+                response.status(400).json('Incorrect username/password');
+        });
+    }
     else
-        res.status(400).json('Incorrect username/password');
+        response.status(400).json('Incorrect username/password');
 })
 
 app.post('/register', (req, res) => {
     const { name, email, phone, password } = req.body;
-    database.users.push({
-        id: 103,
-        name: name,
-        phone: phone,
-        email: email,
-        password: password,
-        joined: new Date()
+    bcrypt.hash(password, 8, (err, hash) => {
+        if (!err) {
+            database.users.push({
+                id: 103,
+                name: name,
+                phone: phone,
+                email: email,
+                password: hash,
+                joined: new Date()
+            });
+            console.log(database.users);
+            res.status(200).json(database.users[database.users.length - 1]);
+        }
+        else {
+            res.status(400).json('Could not generate hash for password');
+        }
     });
-    console.log(database.users);
-    res.status(200).json(database.users[database.users.length - 1]);
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -64,9 +82,9 @@ app.get('/profile/:id', (req, res) => {
         return user.id === parseInt(req.params.id)
     })
     if (userFound)
-        res.status(200).send(database.users[userFoundAtIndex]);
+        res.status(200).json(database.users[userFoundAtIndex]);
     else
-        res.status(400).send('User not found');
+        res.status(400).json('User not found');
 })
 
 app.listen(3000, () => {
