d11639f03b774b915416df43c471349a70f12bd2
Lokesh Roopkumar <r.lokesh98@gmail.com>
2021-11-24 17:37:38 -0600

    adding sparkline chart

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './Card.css';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 const Card = ({ coins }) => {
     return (
         <div className='card-list'>
             {
                 coins.map((coin) => {
+
+                    const sparklineDataPoints = coin.sparkline.map(point => {
+                        return Number(point)
+                    });
+
                     return (
-                        <div className='card'>
-                            <img alt='crypto icon' src={coin.iconUrl} width="80" height="80" id='crypto-icon-img' />
-                            <div classname='card-text'>
+                        <div key={coin.rank + 300} className='card'>
+                            <img key={coin.rank + 250} alt='crypto icon' src={coin.iconUrl} width="80" height="80" id='crypto-icon-img' />
+                            <div key={coin.rank + 350} className='card-text'>
                                 <p key={coin.rank}>{`Rank: ${coin.rank}`} </p>
-                                <p>{`Name: ${coin.name} ${coin.symbol}`} </p>
-                                <p>{`Price: $ ${Number(coin.price).toFixed(2)}`} </p>
-                                <p>{`Change: ${coin.change}`} </p>
-                                <p>{`Market Cap: ${coin.marketCap}`} </p>
+                                <p key={coin.rank + 50}>{`Name: ${coin.name} ${coin.symbol}`} </p>
+                                <p key={coin.rank + 100}>{`Price: $ ${Number(coin.price).toFixed(2)}`} </p>
+                                <p key={coin.rank + 150}>{`Change: ${coin.change}`} </p>
+                                <p key={coin.rank + 200}>{`Market Cap: ${coin.marketCap}`} </p>
                             </div>
+                            <Sparklines data={sparklineDataPoints} limit={27} width={70} height={20} margin={3}>
+                                <SparklinesLine color="blue" style={{ fill: "none" }} />
+                            </Sparklines>
                         </div>
                     );
                 })
