c832382777457e175e0b1898b0b89584d2c49d81
Lokesh Roopkumar <r.lokesh98@gmail.com>
2021-11-24 16:37:26 -0600

    fixing spaces

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -10,9 +10,9 @@ const Card = ({ coins }) => {
                         <div className='card'>
                             <img alt='crypto icon' src={coin.iconUrl} width="80" height="80" id='crypto-icon-img' />
                             <div classname='card-text'>
-                                <p>{`Rank: ${coin.rank}`} </p>
+                                <p key={coin.rank}>{`Rank: ${coin.rank}`} </p>
                                 <p>{`Name: ${coin.name} ${coin.symbol}`} </p>
-                                <p>{`Price: ${coin.price}`} </p>
+                                <p>{`Price: $ ${Number(coin.price).toFixed(2)}`} </p>
                                 <p>{`Change: ${coin.change}`} </p>
                                 <p>{`Market Cap: ${coin.marketCap}`} </p>
                             </div>
